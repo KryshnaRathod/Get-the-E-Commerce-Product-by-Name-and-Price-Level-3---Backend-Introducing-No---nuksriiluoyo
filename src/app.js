bbd3b0caeca9e13e41d55f4924576587604d4f57
Krishna Rathod <88035445+KryshnaRathod@users.noreply.github.com>
2023-07-19 11:05:18 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,10 +5,31 @@ const app = express();
 // Importing products from products.json file
 const products = JSON.parse(fs.readFileSync(`${__dirname}/data/products.json`));
 
-// Middlewares
+//Middlewares
 app.use(express.json());
 
-// Write GET endpoint for sending product to the client here
-// Endpoint - /api/v1/products/:name/:price
+// GET endpoint for sending the products to client by id
+//// Endpoint - /api/v1/products/:id
+app.get("/api/v1/products/:id", (req, res) => {
+    const ix = req.params.id;
+    console.log(ix);
+    let val = products.find((item) => {
+        return item.id == ix;
+    });
+
+    if (val) {
+        console.log(val);
+        res.status(200).json({
+            status: "success",
+            message: "Product fetched successfully",
+            data: {
+                product: val
+            }
+        });
+    } 
+    else {
+        res.status(404).json({ status: "failed", message: "Product not found!" });
+    }
+});
 
 module.exports = app;
